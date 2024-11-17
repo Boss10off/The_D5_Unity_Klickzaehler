1feab3239247b7c74eb533b9765c073801760bba
nishan-singh <singh007nishan@gmail.com>
2024-11-17 21:57:00 +0100

    update a bunch of stuff like css styling and add listing

--- js-version/app.js
+++ js-version/app.js
@@ -1,12 +1,47 @@
-const countValueParaElement = document.querySelector('.count-value');
-const increaseBtn = document.querySelector('.count-increase-btn') ;
+const counterWrapper = document.querySelector(".counter-wrapper");
+const addNewCounterBtn = document.querySelector('.add-new-counter-btn');
+const getSavedCounterBtn = document.querySelector('.load-saved-counter-btn');
+const overlayElm = document.getElementById("overlay");
+
+const countValueParaElement = counterWrapper.querySelector('.count-value');
+const resetBtn = counterWrapper.querySelector('#reset-list-btn');
+const increaseBtn = counterWrapper.querySelector('.count-increase-btn') ;
+
 const catSVG = document.querySelector('.cat-svg') ;
-let countValue = 0; 
+
+let countTotal = 0;
+
+// Fixed name to storage different counters (just like an Enum)
+const counterListNameForLocalStorage = "countersList";
+
+const countersList = [
+    {
+        name: 'cats',
+        count: 10
+    },
+    {
+        name: 'dogs',
+        count: 90
+    },
+];
+
+let dialogOpen = false;
+
+window.onload = () => {
+    const wrapperProperties = counterWrapper.getBoundingClientRect();
+    const wrapperTop = wrapperProperties.top;
+    const wrapperLeft = wrapperProperties.left;
+    const wrapperWidth = wrapperProperties.width;
+    catSVG.style.top = (wrapperTop - 102) + "px";
+    catSVG.style.left = (wrapperLeft + (wrapperWidth / 2.3)) + "px";
+    if(!localStorage.getItem(counterListNameForLocalStorage)) {
+        localStorage.setItem(counterListNameForLocalStorage, JSON.stringify(countersList));
+    }
+}
 
 increaseBtn.addEventListener('click', () => {
-    const newNumber = document.createElement("span");
-    newNumber.textContent = (countValue + 1).toString().padStart(4, '0');
-    newNumber.classList.add("show");
+    const newNumber = createElementWithClass('span', 'show');
+    newNumber.textContent = (countTotal + 1).toString().padStart(4, '0');
 
     // Add current number and apply the move-up animation
     const currentNumber = countValueParaElement.querySelectorAll("span");
@@ -17,7 +52,7 @@ increaseBtn.addEventListener('click', () => {
     // Append the new number and increment count
     countValueParaElement.appendChild(newNumber);
     catSVG.querySelectorAll('.right-leg').forEach(e => e.classList.add("animate-right-leg"))
-    countValue++;
+    countTotal++;
 
     // Clean up old numbers after animation
     setTimeout(() => {
@@ -29,3 +64,154 @@ increaseBtn.addEventListener('click', () => {
         } 
     }, 300);
 });
+
+resetBtn.addEventListener("click", () => {
+    const newNumber = createElementWithClass('span', 'show');
+    countTotal = 0;
+    newNumber.textContent = (countTotal).toString().padStart(4, '0');
+
+    countValueParaElement.innerHTML = "";
+    countValueParaElement.appendChild(newNumber);
+})
+
+addNewCounterBtn.addEventListener("click", () => {
+    toggleOverlay();
+    const dialog = createElementWithClass('article', 'dialog');
+    const heading = createElementWithClassAndText('h2', 'dialog-heading', "Neuen Zähler erstellen");
+    const inputsForm = createElementWithClass('form', 'inputs-form');
+
+    const counterNameInputField = createInputFieldRequired('Name eingeben', 'name-input-field')
+
+    const btnsWrapper = createElementWithClass('div', 'btns-wrapper');
+    const cancelBtn = createElementWithClassAndText('button', 'cancel-btn', 'abbrechen');
+    const saveBtn = createElementWithClassAndText('button', 'save-btn', 'speichern');
+
+    btnsWrapper.appendChild(cancelBtn);
+    btnsWrapper.appendChild(saveBtn);
+
+    inputsForm.appendChild(counterNameInputField);
+    inputsForm.appendChild(btnsWrapper);
+
+    dialog.appendChild(heading);
+    dialog.appendChild(inputsForm);
+    overlay.appendChild(dialog);
+
+    inputsForm.addEventListener("submit", (e) => addNewCounter(e, counterNameInputField, overlayElm))
+
+    cancelBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        closeDialog(overlayElm);
+    })
+
+})
+
+// @overlay reference to hide it
+
+function addNewCounter(e, inputField, overlay) {
+    e.preventDefault();
+    const counterName = inputField.value;
+    if(counterName.length > 0) {
+        countersList.push({
+            name: counterName,
+            count: countTotal
+        });
+        localStorage.setItem(counterListNameForLocalStorage, JSON.stringify(countersList));
+    }
+
+    closeDialog(overlay);
+}
+
+getSavedCounterBtn.addEventListener('click', () => {
+    toggleOverlay();
+    const dialog = createElementWithClass('article', 'dialog');
+    const heading = createElementWithClassAndText('h2', 'dialog-heading', "Wählen Sie einen Zähler aus");
+
+    const savedCountersList = JSON.parse(localStorage.getItem(counterListNameForLocalStorage));
+    const countersList = createCountersList(savedCountersList);
+
+    dialog.appendChild(heading);
+    dialog.appendChild(countersList);
+
+    dialog.addEventListener("click", (e) => {
+        const displayCountBtn = e.target.closest("button.display-counter-btn");
+        if(displayCountBtn !== null) {
+            const counterName = displayCountBtn.querySelector('.each-counter-name').textContent;
+            const counterCount = displayCountBtn.querySelector('.each-counter-count').textContent;
+            updateUIWithClickCounter(counterName, counterCount);
+            closeDialog(overlay);
+        }
+    })
+
+    overlay.appendChild(dialog);
+})
+
+function updateUIWithClickCounter(counterName, counterValue) {
+    const newNumber = createElementWithClass('span', 'show');
+    countTotal = parseInt(counterValue);
+    newNumber.textContent = (countTotal).toString().padStart(4, '0');
+
+    countValueParaElement.innerHTML = "";
+    countValueParaElement.appendChild(newNumber);
+
+}
+
+// helper functions
+
+function toggleOverlay() {
+    overlay.style.display = "flex";
+    dialogOpen = true;
+    closeOnOutsideClick(dialogOpen);
+}
+
+function createCountersList(counterArray) {
+    const unorderedList = createElementWithClass('ul', 'counters-list');
+    for(let i = 0; i < counterArray.length; i++) {
+        const listItemElm = createElementWithClass('li', 'each-counter');
+        const btnToDisplayCounter = createElementWithClass('button', 'display-counter-btn');
+        const counterNameElm = createElementWithClassAndText('p', 'each-counter-name', counterArray[i].name);
+        const counterCount = createElementWithClassAndText('p', 'each-counter-count', counterArray[i].count);
+        btnToDisplayCounter.appendChild(counterNameElm);
+        btnToDisplayCounter.appendChild(counterCount);
+        listItemElm.appendChild(btnToDisplayCounter);
+        unorderedList.appendChild(listItemElm);
+    }
+    return unorderedList;
+}
+
+function createElementWithClass(elmName, classToAdd) {
+    const elm = document.createElement(elmName);
+    elm.classList.add(classToAdd);
+    return elm;
+}
+
+function createElementWithClassAndText(elmName, classToAdd, text) {
+    const elm = createElementWithClass(elmName, classToAdd);
+    elm.textContent = text;
+    return elm;
+}
+
+function createInputFieldRequired(placeholderText, classToAdd) {
+    const elm = createElementWithClassAndText('input', classToAdd);
+    elm.placeholder = placeholderText;
+    elm.required = true;
+    return elm;
+}
+
+function closeOnOutsideClick(dialogOpen) {
+    if(dialogOpen) {
+        document.onclick = (e) => {
+            if(e.target.id === 'overlay') {
+                overlay.style.display = "none";
+                overlay.innerHTML = '';
+                dialogOpen = false;
+            }
+        }
+    }
+}
+
+function closeDialog(overlay) {
+    overlay.style.display = "none";
+    overlay.innerHTML = '';
+    dialogOpen = false;
+}
+
